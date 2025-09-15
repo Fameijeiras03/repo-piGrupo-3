d936601a5b8ba8d25343e459c250e5a1443ee18c
Fameijeiras03 <fameijeiras@udesa.edu.ar>
2025-09-15 18:10:50 -0300

    Index Router

--- piGrupo3/routes/index.js
+++ piGrupo3/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+const mC = require('../controllers/mainController')
+
+router.get('/', mC.index)
+router.get('/index', mC.index)
+router.get('/search', mC.search)
+
+
+
+
 module.exports = router;
